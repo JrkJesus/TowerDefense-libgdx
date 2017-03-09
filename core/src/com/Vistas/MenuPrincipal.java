9a5ad41053e999366bffd3aca172b7cca3fb9495
AntonioRuzPerez <ruzperezantonio@gmail.com>
2017-03-09 18:59:06 +0100

    Añadidas PantallaBase y MainScreen
    PantallaBase implementa screen y le he añadido las funciones de screen y
    un constructor
    
    MainScreen hereda de PantallaBase

---
+++ core/src/com/Vistas/MenuPrincipal.java
@@ -0,0 +1,17 @@
+package com.Vistas;
+
+import com.towerdeffense.MainTowerDeffense;
+
+/**
+ * Created by skarg on 09/03/2017.
+ */
+
+public class MenuPrincipal extends PantallaBase
+{
+
+    public MenuPrincipal(MainTowerDeffense m) {
+        super(m);
+    }
+
+
+}
---
+++ core/src/com/Vistas/PantallaBase.java
@@ -0,0 +1,56 @@
+package com.Vistas;
+
+import com.badlogic.gdx.Screen;
+import com.towerdeffense.MainTowerDeffense;
+
+/**
+ * Created by skarg on 09/03/2017.
+ */
+
+public class PantallaBase implements Screen
+{
+    protected MainTowerDeffense mtd;
+
+    public PantallaBase(MainTowerDeffense m)
+    {
+        this.mtd = M;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+
+}
