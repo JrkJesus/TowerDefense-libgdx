092d9130b6c63928cb8d9481d435def0f486465c
JrkJesus <jesus21hlv@gmail.com>
2017-03-16 10:45:34 +0100

    Añadida mejora resolucion
    Aumentada la resolucion
    Cambiado el aspect Ratio a 16:9
    Añadido un viewport
    Añadido método para poner botones y limpiar codigo.

--- core/src/com/Vistas/PantallaBase.java
+++ core/src/com/Vistas/PantallaBase.java
@@ -19,13 +19,17 @@ import com.towerdeffense.MainTowerDeffense;
 public class PantallaBase implements Screen
 {
     protected final MainTowerDeffense mtd;
-    OrthographicCamera cam;
+    protected OrthographicCamera cam;
+    protected Viewport view;
 
     public PantallaBase(MainTowerDeffense _mtd)
     {
         mtd = _mtd;
+        // cam = new OrthographicCamera(1366, 768);
         cam = new OrthographicCamera();
-        cam.setToOrtho(false, 800, 480);
+        cam.setToOrtho(false, 1280, 720); // Asigno una vista de mi mapa.
+        cam.rotate(90); // Ponemos la vista en vertical.
+        new FitViewport(1280, 720, camera);
     }
 
     protected boolean isButtonPress(Texture btn, int x, int y) {
@@ -45,7 +49,12 @@ public class PantallaBase implements Screen
         font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         font.getData().setScale(scale);
     }
-
+    
+    protected void addButton(Texture btn, String text, int x, int y, int marginLeft, int marginBottom){
+        mtd.batch.draw(btn, x, y);
+        mtd.font.draw(mtd.batch, text, x + marginLeft, y + marginBottom);
+    
+    }
     @Override
     public void show() {
 
@@ -58,7 +67,15 @@ public class PantallaBase implements Screen
 
     @Override
     public void resize(int width, int height) {
-
+        // Vector2 size = Scaling.fit.apply(800, 480, width, height);
+        // int viewportX = (int)(width - size.x) / 2;
+        // int viewportY = (int)(height - size.y) / 2;
+        // int viewportWidth = (int)size.x;
+        // int viewportHeight = (int)size.y;
+        // Gdx.gl.glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
+        // stage.setViewport(800, 480, true, viewportX, viewportY, viewportWidth, viewportHeight);
+        view.update(width, height);
+        camera.update();
     }
 
     @Override
