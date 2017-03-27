81ddae92f6898994ca65f5ee9ca935136e1953ad
JrkJesus <jesus21hlv@gmail.com>
2017-03-27 11:52:13 +0200

    Update Enemy Sprite & function to Turret
    Funcion de ataque de las torretas
    Funcion eliminar torreta
    Funcion añadir torreta
    Actualización al nuevo enemigo que hereda de sprite

--- core/src/com/view/PantallaJuego.java
+++ core/src/com/view/PantallaJuego.java
@@ -24,9 +24,12 @@ public class PantallaJuego extends PantallaBase {
      //TODO: 21/03/2017 ver si se puede cambiar el fondo por Actor y asi no hay que llamar al mtd.
             // ademas si lo declaramos touchable sería mas facil a la hora de poner las torretas.
     private Texture fondo;
-    private Texture[] machineGun, antiAir, antiTank, plane, tank, people;
+    private Texture[] machineGun, antiAir, antiTank;
+    private Button btnMachine, btnAntiAir, bntAntiTank;
+    private 
+    private Sprite plane, tank, people;
     private Array<Enemy> enemies;
-    private Array<Turret> turrets;
+    //private Array<Turret> turrets;
     private Stage stage;
     private int life, money;
     
@@ -71,10 +74,6 @@ public class PantallaJuego extends PantallaBase {
                 enemies.add(new Enemy(this, plane, Constants.PATH_PLANE(), (rnd.nextInt(4)+4)*100, Constants.PLANE));
             break;
         }
-        for(Enemy enemy : enemies){
-            enemy.addAction(parallel(rotateTo(90.0f,5f)));
-            stage.addActor(enemy);
-        }
         life = 10;
         Gdx.input.setInputProcessor(stage);
         //TODO: 21/03/2017 la vida tiene que depender de la dificultad elegida.
@@ -83,14 +82,14 @@ public class PantallaJuego extends PantallaBase {
     public void initTextures(int fase){
         fondo = new Texture(Gdx.files.internal("Paths\\path_stage"+fase+".png"));
         
-        plane = new Texture[]{new Texture(Gdx.files.internal("Textures\\plane.png")), 
-                              new Texture(Gdx.files.internal("Textures\\planeDead.png")) };
+        plane = new Sprite( new Texture(Gdx.files.internal("Textures\\plane.png")) );
+         //                     new Texture(Gdx.files.internal("Textures\\planeDead.png")) };
         
-        tank = new Texture[]{new Texture(Gdx.files.internal("Textures\\tank.png")), 
-                             new Texture(Gdx.files.internal("Textures\\tankDead.png")) };
+        tank = new Sprite( new Texture(Gdx.files.internal("Textures\\tank.png")) );
+         //                    new Texture(Gdx.files.internal("Textures\\tankDead.png")) };
         
-        people = new Texture[]{new Texture(Gdx.files.internal("Textures\\people.png")), 
-                               new Texture(Gdx.files.internal("Textures\\peopleDead.png")) };
+        people = new Sprite( new Texture(Gdx.files.internal("Textures\\people.png")) );
+         //                      new Texture(Gdx.files.internal("Textures\\peopleDead.png")) };
         
         antiAir = new Texture[]{new Texture(Gdx.files.internal("Textures\\antiAirLv1.png")), 
                                new Texture(Gdx.files.internal("Textures\\antiAirLv2.png")),
@@ -112,26 +111,56 @@ public class PantallaJuego extends PantallaBase {
 
         mtd.batch.begin();
         mtd.batch.draw(fondo, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() );
+         //turretsAttack();
+        for( Enemy enemy : enemies)
+             enemy.draw(mtd.batch);
         mtd.batch.end();
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
-
-        for(Enemy enemy : enemies) {
-            for (Iterator<Action> iter = enemy.getActions().iterator(); iter.hasNext(); ) {
-                iter.next().act(delta);
-            }
+    
+        if( life <= 0 ){
+             //TODO: 27/03/2017 pantalla game over
+        } else if ( enemies.size == 0 ) {
+            //TODO: 27/03/2017 pantalla ganadora
         }
-        //TODO: 21/03/2017 controlar de alguna forma la destruccion de enemigos
-            // mejor forma de hacer pasandole un objeto al enemigo y que llame a metodos de aqui.
 
     }
-
-    private void eliminateEnemy(Enemy enemy){
-        for(Actor e : stage.getActors()){
-            if(enemy == e)
-                e.remove();
-        }
+    //TODO: 27/03/2017 sobrecargar el touchdown para que pinte las torres
+     
+    private turretsAttack(){
+       for(Actor turret : stage.getActors()){
+          if( !turret.isReloading() ){
+               int i = 1;
+               Enemy enemy = enemies.get(0);
+               while( !enemy.isAttackable() && !turret.isReachable(enemy.getPosition()) )
+                    enemy = enemies.get(i++);
+               enemy.reciveDamage(turret.getAttack());
+               turret.reload();
+          }
+       }
+    }
+    
+    private void addMoney(int amount){
+          money += amount;
+    }
+     
+    public void addTurret(Turret turret){
+         stage.addActor(turret);
+    }
+     
+    public void eliminateTurret(Turret eliminate){
+         //TODO: 27/03/2017 hacerlo con un while
+         for(Actor turret : stage.getActors()){
+              if(turret.equals(eliminate)){
+                   //TODO: 27/03/2017 quitar del stage turret
+                   addMoney( (int) (turret.getValue() / 3.5) );
+              }
+         }
+    }
+    
+    public void eliminateEnemy(Enemy enemy){
+       enemies.removeValue(enemy, false);
     }
 
     public void loseLife(Enemy enemy){
@@ -150,11 +179,9 @@ public class PantallaJuego extends PantallaBase {
             machineGun[i].dispose();
         }
         
-        for(int i = 0; i < 2; i++){
-            plane[i].dispose();
-            tank[i].dispose();
-            people[i].dispose();
-        }
+        plane.getTexture().dispose();
+        tank.getTexture().dispose();
+        people.getTexture().dispose();
         
     }
 }
